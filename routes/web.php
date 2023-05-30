25ca503790a856330493199c55274b242b3e83d7
Eding Muhamad <masaruedo@yahoo.com>
2023-05-30 16:38:12 +0700

    Remove Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class)->middleware('guest');
     Route::post('/logout', LogoutController::class)->middleware('auth');
